f0d0ab69b1c7b5ec087e18409947e3e01bdc6853
Joe Bottigliero <joe@bottigliero.com>
2012-11-01 16:04:44 -0500

    Laying out basic functionality...

--- src/mod_prebind.erl
+++ src/mod_prebind.erl
@@ -11,22 +11,54 @@
 %%% ejabberd Globals
 -include("ejabberd.hrl").
 
+%%% HTTP Globals
+-include("/root/ejabberd/src/web/http_bind.hrl").
+
 %%% HTTP Record
 -include("web/ejabberd_http.hrl").
 
+
 -define(JABBER_DOMAIN, "example.org").
 
--export([
-  process/2
-]).
+-export([process/2]).
+
 
 process([], #request{ method = 'POST',
-                      auth = Auth,
+                      %% auth = Auth,
                       data = Data,
                       ip = IP}) ->
-
-  ?DEBUG("Incoming data: ~s", [Data]).
+  ?DEBUG("Incoming data: ~s", [Data]),
+  process_request(Data, IP);
 
 process(_Path, _Request) ->
     ?DEBUG("Bad Request: ~p", [_Request]),
-    {403, [], "Forbidden"}.
+    {400, ?HEADER, "Bad Request."}.
+
+process_request(Data, IP) ->
+  case authorized(Data) of 
+    {Username, Server} ->
+      bind(Username, Server, IP);
+    _ ->
+      ?DEBUG("Unauthorized Request ~s", [Data])
+  end.
+  
+authorized(Data) ->
+    case Data of 
+      {Username, Token} ->
+        case jlib:string_to_jid(Username) of
+          error ->
+              unauthorized;
+          #jid{user = U, server = S} ->
+              case ejabberd_auth:check_password(U, S, Token) of
+                  true ->
+                      {U, S};
+                  false ->
+                      unauthorized
+              end
+        end;
+      _ ->
+        unauthorized
+    end.
+
+bind(Username, Server, IP) ->
+  ?DEBUG("Starting Pre-Bind Process", []).
